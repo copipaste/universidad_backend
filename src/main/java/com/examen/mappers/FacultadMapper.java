2919beb737810edef4fc28fab52d106888abf8cc
NielsRoy <nielsroy8@gmail.com>
2024-06-09 14:40:36 -0400

    crud completo con validaciones

--- src/main/java/com/examen/mappers/FacultadMapper.java
+++ src/main/java/com/examen/mappers/FacultadMapper.java
@@ -4,11 +4,13 @@ import com.examen.dtos.facultad.CreateFacultadDto;
 import com.examen.dtos.facultad.FacultadDto;
 import com.examen.entities.Facultad;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface FacultadMapper {
 
     FacultadDto toFacultadDto(Facultad facultad);
 
+    @Mapping(target = "active", constant = "1")
     Facultad toFacultad(CreateFacultadDto createFacultadDto);
 }
