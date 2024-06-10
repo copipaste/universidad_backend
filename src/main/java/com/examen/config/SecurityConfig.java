70367fc9d54ed632f5aa24d76edf72da73a28992
NielsRoy <nielsroy8@gmail.com>
2024-06-10 19:02:40 -0400

    api documentation with swagger

--- src/main/java/com/examen/config/SecurityConfig.java
+++ src/main/java/com/examen/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((requests) ->
                         requests.requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
+                                .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                                 .anyRequest().authenticated()
                 );
         return http.build();
