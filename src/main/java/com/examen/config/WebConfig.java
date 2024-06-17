4a9ea107ea1f320bd5ab339d9e89da521da7f5b9
copipaste <jhoeldebrayquispe@gmail.com>
2024-06-17 16:25:44 -0400

    conf despliegue 2

--- src/main/java/com/examen/config/WebConfig.java
+++ src/main/java/com/examen/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("/**");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
